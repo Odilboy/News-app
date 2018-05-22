acfec608c65bcb2aaf66b56a96c062066e2413e3
odiljonergashev <oaergashev@gmail.com>
2018-05-22 17:51:29 +0900

    Merge branch 'master' of https://bitbucket.org/odiljon1994/news-app into secondCommit
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/example/ergashevodiljon/newsapp/MainActivity.java
    #	app/src/main/java/com/example/ergashevodiljon/newsapp/NewsApi.java
    #	app/src/main/java/com/example/ergashevodiljon/newsapp/RetrofitClientInstance.java

--- app/src/main/java/com/example/ergashevodiljon/newsapp/SecondActivity.java
+++ app/src/main/java/com/example/ergashevodiljon/newsapp/SecondActivity.java
@@ -1,4 +1,19 @@
 package com.example.ergashevodiljon.newsapp;
 
-public class SecondActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class SecondActivity extends AppCompatActivity{
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read_news);
+        webView = findViewById(R.id.web_view);
+        webView.loadUrl("https://www.hrupin.com/2012/08/how-to-open-activity-by-android-webview-hyperlink-click-or-how-to-handle-hyperlink-click-in-android-webview");
+        webView.setWebViewClient(new MyWebViewClient(this));
+    }
 }
