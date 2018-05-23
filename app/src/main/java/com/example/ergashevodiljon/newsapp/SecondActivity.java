37fd3eecbb0f5a78ef70a1b65d21e4a075483516
odiljonergashev <oaergashev@gmail.com>
2018-05-23 15:39:50 +0900

    Merge branch 'master' of https://bitbucket.org/odiljon1994/news-app into secondCommit
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/example/ergashevodiljon/newsapp/MainActivity.java
    #	app/src/main/java/com/example/ergashevodiljon/newsapp/NewsApi.java
    #	app/src/main/java/com/example/ergashevodiljon/newsapp/RetrofitClientInstance.java

--- app/src/main/java/com/example/ergashevodiljon/newsapp/SecondActivity.java
+++ app/src/main/java/com/example/ergashevodiljon/newsapp/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.ergashevodiljon.newsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
@@ -15,5 +16,14 @@ public class SecondActivity extends AppCompatActivity{
         webView = findViewById(R.id.web_view);
         webView.loadUrl(getIntent().getStringExtra("data"));
         webView.setWebViewClient(new MyWebViewClient(this));
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        setContentView(R.layout.activity_main);
+        super.onBackPressed();
     }
 }
